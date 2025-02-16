2df0aa96e615536aa5ff660c1a8db0cae2c61689
bscit-05-39008695 <aronmutai99@gmail.com>
2025-02-16 17:41:31 +0300

    adds

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { dirname, resolve } from 'path';import { fileURLToPath } from 'url';
+import tailwindcss from '@tailwindcss/vite'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'), // This properly resolves @ to the src directory
+      '@': '/src', // This will resolve @ to the src directory
     },
   },
-  // Add base configuration for proper path handling
-  base: '/',
-  // Ensure proper build settings for SPA
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-  }
 });
+
+
