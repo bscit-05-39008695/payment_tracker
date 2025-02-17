7dd2f8122ba5b9fba649248c6a81c985fab88b8a
bscit-05-39008695 <aronmutai99@gmail.com>
2025-02-17 21:42:23 +0300

    adds

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -26,7 +26,7 @@ const LoginPage = () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('https://paxful-backend-4.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
