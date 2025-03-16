2c55e54eeb432fb9e107429ba3368ef950e6f7ac
bscit-05-39008695 <aronmutai99@gmail.com>
2025-03-16 15:05:44 +0300

    adds

--- src/components/PaymentTracking.jsx
+++ src/components/PaymentTracking.jsx
@@ -19,7 +19,7 @@ const PaymentTracking = () => {
         <div className="panel" >
           <Circle className="circle blue"/>
           <div>
-            <p className="panel-text">Payment of INR 10,000.00 made by the buyer from their BANK ACCOUNT via PaxfulPay</p>
+            <p className="panel-text">Payment of INR 5,000.00 made by the buyer from their BANK ACCOUNT via PaxfulPay</p>
             <p className="panel-date">16 March 2025</p>
           </div>
         </div>
@@ -28,7 +28,7 @@ const PaymentTracking = () => {
         <div className="panel">
           <Circle className="circle yellow"/>
           <div>
-            <p className="panel-text">Transaction processed successfully by PaxfulPay to be deposited in your ACCOUNT TO Satish Bairwa, 61138459952
+            <p className="panel-text">Transaction processed successfully by PaxfulPay to be deposited in your ACCOUNT TO Harsh, 2157001500029363
             account</p>
             <p className="panel-date">16 March 2025</p>
           </div>
