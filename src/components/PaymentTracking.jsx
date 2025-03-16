b13b846adb39d8b3b2a924a72b9bd5ab8db27621
bscit-05-39008695 <aronmutai99@gmail.com>
2025-03-16 13:58:51 +0300

    adds

--- src/components/PaymentTracking.jsx
+++ src/components/PaymentTracking.jsx
@@ -19,8 +19,8 @@ const PaymentTracking = () => {
         <div className="panel" >
           <Circle className="circle blue"/>
           <div>
-            <p className="panel-text">Payment of PHP 40,000.00 made by the buyer from their BANK ACCOUNT via PaxfulPay</p>
-            <p className="panel-date">12 March 2025</p>
+            <p className="panel-text">Payment of INR 262.00 made by the buyer from their BANK ACCOUNT via PaxfulPay</p>
+            <p className="panel-date">16 March 2025</p>
           </div>
         </div>
 
@@ -28,9 +28,9 @@ const PaymentTracking = () => {
         <div className="panel">
           <Circle className="circle yellow"/>
           <div>
-            <p className="panel-text">Transaction processed successfully by PaxfulPay to be deposited in your ACCOUNT TO Judy Ann Simogan Moniz,  2086 7603 8309 38
+            <p className="panel-text">Transaction processed successfully by PaxfulPay to be deposited in your ACCOUNT TO Hemant Jalutharia, hemant77710@ybl
             account</p>
-            <p className="panel-date">12 March 2025</p>
+            <p className="panel-date">16 March 2025</p>
           </div>
         </div>
 
